22e8a792499541a7a84c1db06d8f07632c2606e6
LC <1131435616@qq.com>
2021-01-25 20:46:44 +0800

    解决日志保存 图片验证码 访问信息失败问题

--- sql/update_0115.sql
+++ sql/update_0115.sql
@@ -9,3 +9,8 @@ alter table o_role_menu
 -- tenant库
 alter table t_role_menu
     add nav_platform int default 1 null comment '导航平台 0-不生成导航，1-pc端导航，2-移动端导航';
+	
+	
+-- operate库
+-- 日志出参格式修改
+ALTER TABLE sys_log modify out_param varchar(8192) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '出参';
